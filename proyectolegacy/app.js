9d82a2d6e8d363d134f110c2c12a9c1d72bf9284
David Pestana Perdomo <david.pestana.perdomo@gmail.com>
2025-09-11 17:48:02 +0000

    iteracion

---
+++ proyectolegacy/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/health", (_req, res) => res.json({ status: "ok" }));
+app.get("/", (_req, res) => res.send("Hello from Express!!"));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on http://0.0.0.0:${PORT}`);
+});
